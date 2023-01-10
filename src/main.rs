23a62c6c3397dc8d35d5dc46bcb373dbf1eb517d
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-10 01:52:03 +0000

    Fix error, change book to library in insert command

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     // create a connection to a db, if db does not exist it will be created
     let conn = Connection::open("books.db").unwrap();
 
-    conn.execute(
+    let _ = conn.execute(
         "CREATE TABLE library(id INTEGER PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL)",
         (),
     );
@@ -22,8 +22,8 @@ fn main() {
         author: "J. G. Ballard".to_string(),
     };
 
-    conn.execute(
-        "INSERT INTO book (id, title, author) VALUES",
+    let _ = conn.execute(
+        "INSERT INTO library (id, title, author) VALUES (?1, ?2, ?3)",
         (
             &cocaine_nights.id,
             &cocaine_nights.title,
@@ -35,12 +35,12 @@ fn main() {
         .prepare("SELECT id, title, author FROM library")
         .unwrap();
 
-    let mut book_iter = data
+    let book_iter = data
         .query_map([], |row| {
             Ok(Book {
                 id: row.get(0).unwrap(),
-                title: row.get(0).unwrap(),
-                author: row.get(0).unwrap(),
+                title: row.get(1).unwrap(),
+                author: row.get(2).unwrap(),
             })
         })
         .unwrap();
