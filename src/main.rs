301f26e1e6867700260917131f4ac2f0df7b1204
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-10 01:44:26 +0000

    Add book and retreive book

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rusqlite::NO_PARAMS;
 use rusqlite::{Connection, Result};
 
 fn main() {
+    #[derive(Debug)]
     struct Book {
         id: i32,
         title: String,
@@ -14,4 +15,37 @@ fn main() {
         "CREATE TABLE library(id INTEGER PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL)",
         (),
     );
+
+    let cocaine_nights = Book {
+        id: 1,
+        title: "Cocaine Nights ".to_string(),
+        author: "J. G. Ballard".to_string(),
+    };
+
+    conn.execute(
+        "INSERT INTO book (id, title, author) VALUES",
+        (
+            &cocaine_nights.id,
+            &cocaine_nights.title,
+            &cocaine_nights.author,
+        ),
+    );
+
+    let mut data = conn
+        .prepare("SELECT id, title, author FROM library")
+        .unwrap();
+
+    let mut book_iter = data
+        .query_map([], |row| {
+            Ok(Book {
+                id: row.get(0).unwrap(),
+                title: row.get(0).unwrap(),
+                author: row.get(0).unwrap(),
+            })
+        })
+        .unwrap();
+
+    for book in book_iter {
+        println!("{:?}", book.unwrap());
+    }
 }
